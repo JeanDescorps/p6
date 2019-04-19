92c9070611e84641183c63a5f65fe7dd12c82c7e
JeanD34 <jean.webdev@gmail.com>
2019-04-19 17:47:08 +0200

    Last trick on admin dashboard

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\CommentRepository;
 use App\Repository\TrickRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,11 +12,13 @@ class AdminController extends AbstractController
     /**
      * @Route("/admin", name="admin_dashboard")
      */
-    public function dashboard(TrickRepository $repo)
+    public function dashboard(TrickRepository $repoTrick, CommentRepository $repoComment)
     {
-        $tricks = $repo->findBy([], ['id' => 'DESC'], 4);
+        $tricks = $repoTrick->findBy([], ['id' => 'DESC'], 4);
+        $comments = $repoComment->findBy([], ['id' => 'DESC'], 3);
         return $this->render('admin/dashboard.html.twig', [
             'tricks' => $tricks,
+            'comments' => $comments,
         ]);
     }
 }
